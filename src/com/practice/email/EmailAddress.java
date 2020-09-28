5562af9f17361d1ba652bc35162e65e5e3ffe3ec
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-09-28 12:50:04 +0530

    [swapnaneel]UC5:Final expression supporting optional parts

--- src/com/practice/email/EmailAddress.java
+++ src/com/practice/email/EmailAddress.java
@@ -6,7 +6,7 @@ public class EmailAddress {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter email address");
 		String email=sc.nextLine();
-		if(email.matches("^(abc)[_+.-]{0,1}[a-zA-Z]*[@]{1}(bridgelabz)[.]{1}(co)"))
+		if(email.matches("^(abc)[_+.-]{0,1}[a-zA-Z]*[@]{1}(bridgelabz|gmail|yahoo)[.]{1}(co|com|net)[.]{0,1}[a-z]{0,2}"))
 			System.out.println("valid email");
 	}
 }
